3dcc7c181885acbae618d61efec7ff8bd938e28d
Nico <nico.esg.rey@gmail.com>
2025-10-16 00:49:50 +0100

    Programm fully added

---
+++ functions.py
@@ -0,0 +1,289 @@
+from RPA.Excel.Files import Files as Excel
+from robocorp import browser
+from robocorp import windows
+import time
+
+def read_excel_preingreso(path: str, escritos_DCP_Path: str):
+    """
+    open and read an excel file for preingreso
+
+    """
+    print('Preingreso')
+    excel = Excel()
+    excel.open_workbook(path)
+    rows = excel.read_worksheet_as_table("Hoja1", header=True)
+    numero = 2;
+    preingresoIndex = 1;
+    if not rows.columns.__contains__("PREINGRESO") :
+        excel.insert_columns_before(1,1)
+        #preingresoIndex = rows._add_column("PREINGRESO")
+        excel.set_cell_value(1,1,"PREINGRESO")
+        excel.save_workbook(path)
+    else :
+        preingresoIndex = rows.columns.index("PREINGRESO") + 1
+    
+    rows = excel.read_worksheet_as_table("Hoja1", header=True)
+    print(preingresoIndex)
+    for row in rows:
+        if (str(row["PREINGRESO"]) != 'OK') :
+            try :
+               fill_form_escritos(row, escritos_DCP_Path)
+               excel.set_cell_value(numero,preingresoIndex,"OK")
+               print(row["ROL"])
+            except Exception as e :
+               excel.set_cell_value(numero,preingresoIndex,"ERROR")
+               browser.goto("https://ojv.pjud.cl/kpitec-ojv-web/index#facil/tramite")
+               pass
+            
+        excel.save_workbook(path)
+        numero = numero + 1
+   
+    excel.save_workbook(path)
+    excel.close_workbook()
+    print('Done Preingreso')
+
+def fill_form_escritos(row, escritos_DCP_Path: str):
+    
+    page = browser.page()
+    page.get_by_text("Ingresar Escrito").click()
+
+    time.sleep(1)
+
+    page.get_by_text("Competencia").wait_for()
+    page.get_by_text("Competencia").click()
+    page.press("body", "Tab")
+    page.keyboard.type("Civil") 
+    page.press("body", "Tab")
+
+    page.locator("label").filter(has_text="Fijar Datos").locator("path").nth(1).click()
+
+    page.locator("xpath=//label[contains(.,'Tipo')]").wait_for()
+    page.locator("xpath=//label[contains(.,'Tipo')]").click()
+    page.press("body", "Tab")
+    page.locator("xpath=//select[contains(@data-bind,'tiposCausas')]").select_option("option", label="C")
+    time.sleep(2)
+    page.locator("xpath=//label[contains(.,'Tribunal')]").wait_for()
+    page.locator("xpath=//label[contains(.,'Tribunal')]").click()
+    page.press("body", "Tab") 
+    page.press("body", "ArrowDown") 
+
+    tribunal = str(row["TRIBUNAL"])
+    num_tribunal = tribunal[0:len(tribunal)-1]
+    tribunal = str(num_tribunal).strip() + "º juzgado civil de santiago"
+    page.keyboard.type(tribunal) 
+    time.sleep(2)    
+    
+    if int(num_tribunal) == 2:
+        page.press("body", "ArrowDown")
+    elif int(num_tribunal) > 2 and int(num_tribunal) < 10:
+        page.press("body", "ArrowDown")
+        page.press("body", "ArrowDown")
+    
+    time.sleep(1) 
+    page.press("body", "Tab") 
+
+    rol = str(row["ROL"])
+    
+    page.locator("xpath=//label[contains(.,'Rol')]/../input").fill(rol.split('-')[0])
+    page.press("body", "Tab")
+    page.press("body", "Tab")
+    ano = rol.split('-')[1]
+    page.keyboard.type(ano)  
+
+    time.sleep(1)
+    page.locator("xpath=//button[contains(.,' Consulta Rol')]").click()        
+
+    page.locator("xpath=//label[contains(.,'Caratulado')]").wait_for()
+    page.locator("xpath=//label[contains(.,'Caratulado')]").click()
+
+    page.press("body", "Tab")
+    time.sleep(2)
+    """ Selecciono el cuaderno"""
+    page.press("body", "ArrowDown")
+    page.press("body", "Tab")
+
+    time.sleep(2)
+
+    page.keyboard.type("joa") 
+    
+    page.press("body", "Tab")
+    page.press("body", "Tab")
+    page.press("body", "Tab")
+    
+    time.sleep(2)
+    
+    page.keyboard.type("gen")
+     
+    time.sleep(2)
+     
+    page.press("body", "Tab")
+    
+    time.sleep(2)
+
+    page.keyboard.type("da cuenta de pag") 
+    
+    page.press("body", "Tab") 
+  
+    page.locator("xpath=//button[contains(.,' Grabar Escrito')]").click()
+
+    time.sleep(1)
+
+    page.get_by_text("Adjuntar Archivos").wait_for();
+    page.get_by_text("Adjuntar Archivos").click()
+    page.locator("#dDPrincipal").get_by_role("button", name="Adjuntar").click()
+    app = windows.find_window('regex:.*Oficina Judicial Virtual - Portal*')
+    ruta_escrito_operacion = escritos_DCP_Path + "\\" + str(row["MES"]) + "_" + str(row["FECHA"])[0:4]
+    # ruta_escrito_operacion += "\\Trib_" + str(row["TRIBUNAL"])[:-1] + "ų_ROL_" + str(row["ROL"]) + "_"
+    ruta_escrito_operacion += "\\Trib_" + str(row["TRIBUNAL"])[:-1] + "°_ROL_" + str(row["ROL"]) + "_"
+    #ruta_escrito_operacion += "\\Trib_" + str(row["TRIBU"]) + "_ROL_" + str(row["ROL"]) + "_"
+    format_rut = int(row["RUT"])
+    format_rut = f"{format_rut:,}".replace(",", ".") 
+    ruta_escrito_operacion += format_rut + "-" + str(row["DV"]) + ".pdf" 
+    #ruta_escrito_operacion += format_rut + "-" + str(row["DV"]) + "_firmado.pdf"
+
+    app.find('class:"Edit" and name:"Nombre:"').set_value(ruta_escrito_operacion)
+
+    app.find('class:"Button" and name:"Abrir"').click()
+    time.sleep(3)
+    
+    page.get_by_text("Escrito", exact=True).wait_for()
+    page.get_by_text("Escrito", exact=True).click()
+    page.get_by_role("button", name="Cerrar y Continuar").click()
+    
+    page.get_by_text("Trámite Fácil").click()
+
+
+def read_excel_envio(path: str):
+    """
+    open and read an excel file for envio
+
+    """
+    print('Envio')
+    excel = Excel()
+    excel.open_workbook(path)
+    rows = excel.read_worksheet_as_table("Hoja1", header=True)
+    numero = 2;
+    envioIndex = 1;
+    if not rows.columns.__contains__("ENVIO") :
+        excel.insert_columns_before(1,1)
+        #envioIndex = rows._add_column("ENVIO")        
+        excel.set_cell_value(1,1,"ENVIO")
+        excel.save_workbook()
+    else :
+        envioIndex = rows.columns.index("ENVIO") + 1
+    
+    rows = excel.read_worksheet_as_table("Hoja1", header=True)
+    print(envioIndex)
+    for row in rows:
+        if (str(row["ENVIO"]) != 'OK' and str(row["PREINGRESO"]) == 'OK') :
+            try :
+                fill_form_envio(row)
+                excel.set_cell_value(numero,envioIndex,"OK")
+                print(row["ROL"])
+            except :
+                excel.set_cell_value(numero,envioIndex,"ERROR")
+                browser.goto("https://ojv.pjud.cl/kpitec-ojv-web/index#facil/tramite")
+                pass
+            
+        excel.save_workbook()
+        numero = numero + 1
+   
+    excel.save_workbook(path)
+    excel.close_workbook()
+    print('Done Envio')
+
+def fill_form_envio(row):
+
+    page = browser.page()
+    page.get_by_text("Bandeja Escrito").click()
+    
+    time.sleep(2)
+
+    page.locator("xpath=//label[contains(.,'Competencia:')]").wait_for()
+    page.locator("xpath=//label[contains(.,'Competencia:')]").click()
+    page.press("body", "Tab")
+    page.keyboard.type("Civil") 
+    page.press("body", "Tab")
+    time.sleep(1)
+    
+    page.locator("xpath=//label[contains(.,'Tribunal Origen')]").wait_for()
+    page.locator("xpath=//label[contains(.,'Tribunal Origen')]").click()
+    page.press("body", "Tab") 
+    page.press("body", "ArrowDown") 
+
+    tribunal = str(row["TRIBUNAL"])
+    num_tribunal = tribunal[0:len(tribunal)-1]
+    tribunal = str(num_tribunal).strip() + "º juzgado civil de santiago"
+    page.keyboard.type(tribunal) 
+    time.sleep(1)
+    
+    if int(num_tribunal) == 2:
+        page.press("body", "ArrowDown")
+    elif int(num_tribunal) > 2 and int(num_tribunal) < 10:
+        page.press("body", "ArrowDown")
+        page.press("body", "ArrowDown")
+    
+    page.press("body", "Tab") 
+        
+    time.sleep(1)
+    page.press("body", "Tab")
+    time.sleep(1)
+    page.press("body", "Tab")
+    time.sleep(1)
+    page.press("body", "Tab")
+    time.sleep(1)
+    page.press("body", "Space") 
+    page.press("body", "Tab")
+    page.keyboard.type("C") 
+    page.press("body", "Tab")
+
+    rol = str(row["ROL"])   
+    page.keyboard.type(rol.split('-')[0])  
+    page.press("body", "Tab")
+    page.keyboard.type(rol.split('-')[1])  
+    page.press("body", "Tab")
+
+    page.get_by_role("button", name="Consultar Escritos").click()
+
+    time.sleep(2)
+
+    page.locator("xpath=//label[contains(.,'Seleccionar Todo')]").first.click()  
+ 
+    page.locator("xpath=//button[contains(.,'Enviar Poder Judicial')]").click()
+    time.sleep(1)
+    page.locator("#modalConfirmarEnvioSinFirma").wait_for()
+    page.locator("xpath=//div[@id='modalConfirmarEnvioSinFirma']/div/div/div[3]/div/div/div[2]/button").click()
+    time.sleep(2)
+
+    page.get_by_text("Mantenedor").click()
+
+    """
+    Este codigo a continuacion, elimina las operaciones segun el rol
+    
+    page.locator("xpath=//button[contains(.,'Eliminar Escritos')]").click()
+    time.sleep(1)
+    page.locator("#modalConfirmarEnvioSinFirma").wait_for()
+    page.locator("xpath=//div[@id='modalConfirmarEnvioSinFirma']/div/div/div[2]/div/div/div[2]/button").click()
+
+    page.get_by_text("Mantenedor").click()
+    
+    """
+def compare_results() :
+        
+    page = browser.page()
+    page.get_by_text("Bandeja Escrito").click()
+    
+    time.sleep(5)
+
+    page.locator("xpath=//label[contains(.,'Competencia:')]").wait_for()
+    page.locator("xpath=//label[contains(.,'Competencia:')]").click()
+    page.press("body", "Tab")
+    page.keyboard.type("Civil") 
+    page.press("body", "Tab")
+
+    page.locator("a:has-text('Escritos Enviados')").click()
+    time.sleep(2)
+    #page.press("body", "Tab")
+    #page.press("body", "Enter")
+    page.locator("xpath=//button[contains(.,'Exportar Excel')]").click()
+    time.sleep(120)
---
+++ tasks.py
@@ -0,0 +1,221 @@
+from robocorp import browser
+from robocorp.tasks import task
+from RPA.Excel.Files import Files as Excel
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from robocorp import windows
+import os 
+import openpyxl
+from selenium.webdriver.common.keys import Keys
+from openpyxl.utils.exceptions import InvalidFileException
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import PatternFill
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+from pathlib import Path
+import os
+import requests
+import time
+from functions import read_excel_preingreso, read_excel_envio, compare_results
+
+#EXCEL_FILE_NAME = "//DESKTOP-E3HRBD4/Compartidas/Itau_cpago_gtia_estatal/ArchivoInput/Estado Escritos Marzo 2025.xlsx"
+
+EXCEL_FILE_NAME = "C:\\Applications\\RPA 01 - INGRESO DE ESCRITOS DE PAGO GARANTIA ESTATAL EN PODER JUDICIAL\\input\\CPago_96 - sin trib 22.xlsx"
+PATH_BOT = "C:\\Applications\\RPA 01 - INGRESO DE ESCRITOS DE PAGO GARANTIA ESTATAL EN PODER JUDICIAL"
+#ESCRITOS_DCP_PATH = "\\DESKTOP-E3HRBD4\Compartidas\Itau_cpago_gtia_estatal\Escritos Da Cuenta de Pago"
+ESCRITOS_DCP_PATH = "C:\\Applications\\RPA 01 - INGRESO DE ESCRITOS DE PAGO GARANTIA ESTATAL EN PODER JUDICIAL\\input\\documentos"
+ROLES_DONE = []
+
+@task
+def Prueba():
+    print("Prueba de que funciona robocorp")
+
+
+
+@task
+def RPA_01_Ingreso():
+    """
+    RPA 01 - Ingreso de escritos de pago garantia estatal en poder judicial.
+    
+    """
+    browser.configure(
+        browser_engine="chrome",
+        screenshot="only-on-failure",
+        headless=False,
+    )
+    try:
+        page = browser.goto("https://ojv.pjud.cl/kpitec-ojv-web/views/login.html")
+        page.get_by_role("heading", name="Ingreso de demandas y escritos").wait_for()
+        page.get_by_role("button", name="Clave Poder Judicial").first.click()
+        page.get_by_role("textbox", name="Ej: 12345678 (Rut sin guión").click()
+        page.get_by_role("textbox", name="Ej: 12345678 (Rut sin guión").fill("11346197")
+        page.locator("#inputPassword2C").click()
+        page.locator("#inputPassword2C").fill("Picarte1215+")
+        page.get_by_role("button", name="Ingresar").click()
+        page.locator("#roles-modal").get_by_text("Seleccione Perfil").wait_for()
+        page.get_by_text("11.346.197-7").click()
+
+        read_excel_preingreso(EXCEL_FILE_NAME, ESCRITOS_DCP_PATH)
+        
+    finally:
+        # Place for teardown and cleanups
+        # Playwright handles browser closing
+        print('Done')
+    
+@task
+def RPA_01_Envio():
+    """
+    RPA 01 - Ingreso de escritos de pago garantia estatal en poder judicial.
+    
+    """
+    browser.configure(
+        browser_engine="chromium",
+        screenshot="only-on-failure",
+        headless=False,
+    )
+    try:
+        page = browser.goto("https://ojv.pjud.cl/kpitec-ojv-web/views/login.html")
+        page.get_by_role("heading", name="Ingreso de demandas y escritos").wait_for()
+        page.get_by_role("button", name="Clave Poder Judicial").first.click()
+        page.get_by_role("textbox", name="Ej: 12345678 (Rut sin guión").click()
+        page.get_by_role("textbox", name="Ej: 12345678 (Rut sin guión").fill("11346197")
+        page.locator("#inputPassword2C").click()
+        page.locator("#inputPassword2C").fill("Picarte1215+")
+        page.get_by_role("button", name="Ingresar").click()
+        page.locator("#roles-modal").get_by_text("Seleccione Perfil").wait_for()
+        page.get_by_text("11.346.197-7").click()
+
+        read_excel_envio(EXCEL_FILE_NAME)
+
+    finally:
+        # Place for teardown and cleanups
+        # Playwright handles browser closing
+        print('Done')
+
+@task
+def RPA_01_Compare_results():
+    """
+    RPA 01 - Descarga del Excel del poder judicial y comparacion con el Excel de ingreso del bot.
+    """
+    try:
+        chrome_options = Options()
+        download_path = PATH_BOT + '\processing'
+        # For windows:
+        prefs = {'download.default_directory': download_path}
+        chrome_options.add_argument('--start-maximized')
+        #chrome_options.add_argument("--headless=old")
+        chrome_options.add_experimental_option('prefs', prefs)
+        driver = webdriver.Chrome(options=chrome_options)
+        #driver = webdriver.Chrome()
+        driver.get("https://ojv.pjud.cl/kpitec-ojv-web/views/login.html")
+        driver.find_element(By.ID, value='link2C').click()
+        driver.find_element(By.ID, value='inputRut2C').send_keys('11346197')
+        driver.find_element(By.ID, value='inputPassword2C').send_keys('Picarte1215+')
+        driver.find_element(By.CLASS_NAME, value='btn-ingreso-pjud').click()
+        time.sleep(2)
+        driver.find_element(By.CLASS_NAME, value='card-text').click()
+        time.sleep(2)
+        driver.get("https://ojv.pjud.cl/kpitec-ojv-web/index#bandeja/escritos")
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//select[contains(.,'Seleccione Competencia')]/option[text()='Civil']").click()
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//a[contains(.,'Escritos Enviados')]").click()
+
+        time.sleep(5)
+        
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.ARROW_DOWN).perform()
+        
+        driver.find_element(By.XPATH, "//button[contains(.,'Exportar Excel')]").click()
+        time.sleep(15)
+
+        time.sleep(15)         
+        ## Archivo descargado
+        ## Busco el archivo
+        path = PATH_BOT + "\processing"
+        outputPath = PATH_BOT + "\output"
+        dir_list = os.listdir(path)
+        print("Files and directories in '", path, "' :")
+        print(dir_list)
+        file = dir_list[0]
+        path = os.path.join(path, file)
+        informePJUD = openpyxl.load_workbook(path)
+        escritos_enviados_sheet = informePJUD["Escritos Enviados"]
+        get_roles_done_by_excel()
+        # Iterate over the columns in the sheet
+        for column in escritos_enviados_sheet.iter_cols():
+            # Get the value of the first cell in the column
+            # (the cell with the column name)
+            column_name = column[3].value
+            # Check if the column is the "Name" column
+            if column_name == "RIT":
+                # Iterate over the cells in the column
+                for i, cell in enumerate(column):
+                    # Skip the first cell (the cell with the column name)
+                    if i < 4:
+                        continue
+                    # Add the value of the cell to the list
+                    #rit_list.append(cell.value)
+                    trib_cell = "B" + str(i+1)
+                    trib = escritos_enviados_sheet[trib_cell].value.upper().replace("º JUZGADO CIVIL DE SANTIAGO", "")
+                                        
+                    valor = cell.value + "-" + trib
+                    print(cell.value, 'pjud')
+                    if valor in ROLES_DONE :
+                        print(cell.value, 'into roles_done')
+                        escritos_enviados_sheet.cell(cell.row,cell.column, cell.value).fill = PatternFill(start_color='92D050', end_color='92D050', fill_type="solid")
+    
+        # Print the list of rit
+        #print(rit_list, len(rit_list))
+        outputPath = os.path.join(outputPath, 'RESULTADO.xlsx')
+        informePJUD.save(outputPath)
+        ## elimino el archivo
+        #os.remove(path)
+    finally:
+        # Place for teardown and cleanups
+        # Playwright handles browser closing
+        #browser.close();
+        #driver.quit()
+        print('Done')
+
+def get_roles_done_by_excel():
+    """
+    RPA 01 - Review column ENVIO into an Excel file and set the value of ROLES_DONE variable
+    
+    """
+    try:
+        ## Busco el archivo
+        informe_bot = openpyxl.load_workbook(EXCEL_FILE_NAME)
+        informe_bot_sheet = informe_bot["Hoja1"]
+
+        rit_list = []
+        
+        lista = list(informe_bot_sheet.iter_cols());
+        envio_index = None
+        rol_index = None
+        tribunal_index = None
+
+        for index in range(len(lista)):
+            if (lista[index][0].value == 'ENVIO') :
+                envio_index = index
+            elif (lista[index][0].value == 'ROL') :
+                rol_index = index
+            elif (lista[index][0].value == 'TRIBUNAL') :
+                tribunal_index = index        
+        
+        for index in range(informe_bot_sheet.max_row):   
+            if (lista[envio_index][index].value == 'OK') :
+                combinacion = lista[rol_index][index].value+'-'+str(lista[tribunal_index][index].value)[0:len(lista[tribunal_index][index].value)-1]
+                rit_list.append(combinacion)
+            elif (lista[envio_index][index].value == None and lista[rol_index][index].value == None) :
+                break
+
+        global ROLES_DONE
+        ROLES_DONE = list(map(lambda item : "C-"+str(item) ,rit_list))
+        return ROLES_DONE
+
+    finally:
+        informe_bot.close
